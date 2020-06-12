2c650e9bbeb16587c879c1a6008c3bc97af74efa
Adrian Eguez <eadepto@hotmail.com>
2020-06-12 17:19:20 -0500

    variables y console typeof

--- 01-Javascript/01-variables.js
+++ 01-Javascript/01-variables.js
@@ -1,8 +1,24 @@
 // Mutables e Inmutables
+// Mutables
 var numeroUno = 1;
 let numeroDos = 2;
 numeroUno = 5;
 numeroDos = 8;
 numeroUno = false;
 numeroDos = true;
-
+// Inmutables
+const configuracionArchivos = "PDF";
+// configuracionArchivos = "XML";
+// Tipos de variables
+const numero = 1; // number
+const sueldo = 1.2; // number
+const texto = "Adrian"; // string
+const booleanooo = false; // boolean
+const hijos = null; // object
+const zapatos = undefined; // undefined
+console.log(typeof numero);
+console.log(typeof sueldo);
+console.log(typeof texto);
+console.log(typeof booleanooo);
+console.log(typeof hijos);
+console.log(typeof numero);
