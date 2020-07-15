ec1e2eb8efe13360b63670ddeea65bfef3c14291
Adrian Eguez <eadepto@hotmail.com>
2020-07-14 19:16:50 -0500

    Clases

---
+++ 02-typescript/03-funciones.js
@@ -0,0 +1,21 @@
+// 03-funciones.ts
+function sumarNumeros(numeroInicial) {
+    var numerosASumarse = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        numerosASumarse[_i - 1] = arguments[_i];
+    }
+    return numeroInicial;
+}
+sumarNumeros(1, 2, 3, 4, 5);
+functiones;
+imprimir(mensaje, string);
+void {
+    console: console,
+    : .log('Hola ' + mensaje)
+};
+var arregloNumeros = [1, 2, 3];
+var arregloNumerosDos = [1, 2, 3];
+var arregloNumerosTres = [1, 'a', true];
+var arregloNumerosTCuatro = [1, 'a', true];
+var arregloNumerosOTexto = ['1', '2'];
+arregloNumerosOTexto = [1, 2, 3];
