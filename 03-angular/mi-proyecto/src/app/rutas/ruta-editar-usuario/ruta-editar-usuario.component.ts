15bc26d4ca26653937be38412150604fab944196
Adrian Eguez <eadepto@hotmail.com>
2020-09-11 16:44:11 -0500

    hasta ruta usuario

---
+++ 03-angular/mi-proyecto/src/app/rutas/ruta-editar-usuario/ruta-editar-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-editar-usuario',
+  templateUrl: './ruta-editar-usuario.component.html',
+  styleUrls: ['./ruta-editar-usuario.component.css']
+})
+export class RutaEditarUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ 03-angular/mi-proyecto/src/app/rutas/ruta-lista-usuario/ruta-lista-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ruta-lista-usuario',
+  templateUrl: './ruta-lista-usuario.component.html',
+  styleUrls: ['./ruta-lista-usuario.component.css']
+})
+export class RutaListaUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
